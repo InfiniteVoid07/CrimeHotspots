9a676da5b0a50a1db7947cf399346057c6fc1c00
Simarpreet Singh <118796299+Simarpreet7876@users.noreply.github.com>
2023-11-04 23:56:28 +0530

    Added geofencing
    Added geofencing and generating random database

--- backend/newiitm.py
+++ backend/newiitm.py
@@ -1,114 +1,81 @@
 import folium
 import random
-import pandas as pd
 from openrouteservice import Client
+from shapely.geometry import Polygon, Point
 
 # Define the source and destination coordinates
-# ls1 = float(input("Enter the latitude for the source: "))
-# ls2 = float(input("Enter the longitude for the source: "))
-# ld1 = float(input("Enter the latitude for the destination: "))
-# ld2 = float(input("Enter the longitude for the destination:"))
-ls1=28.73886
-ls2=77.0500677
-ld1=28.58281
-ld2=77.08405 
+ls1 = 28.58281
+ls2 = 77.05006
+ld1 = 28.73886
+ld2 = 77.08405
 
 # Create a map
-m = folium.Map(location=[ls1, ls2], zoom_start=8)
+m = folium.Map(location=[ls1, ls2], zoom_start=12.5)
 
 locationsource = f"Latitude: {ls1}, Longitude: {ls2}"
 locationdestin = f"Latitude: {ld1}, Longitude: {ld2}"
 folium.Marker([ls1, ls2], icon=folium.Icon(color='green'), popup=locationsource).add_to(m)
 folium.Marker([ld1, ld2], icon=folium.Icon(color='blue'), popup=locationdestin).add_to(m)
 
-# Initialize empty lists to store the random data
-crime_data = []
+# Create a legend
+legend_html = """
+<div style="position: fixed; 
+     bottom: 50px; left: 50px; width: 150px; height: 150px; 
+     border:2px solid grey; z-index:9999; font-size:14px;
+     background-color: white;
+     opacity: 0.8;
+     ">
+     <p>Legend</p>
+     <p><i class="fa fa-map-marker fa-2x" style="color:green"></i> Source</p>
+     <p><i class="fa fa-map-marker fa-2x" style="color:blue"></i> Destination</p>
+     <p><i class="fa fa-circle fa-2x" style="color:red"></i> Points within Route</p>
+</div>
+"""
 
-# Read the existing data from the CSV file
-existing_data = pd.read_csv("crime_data.csv")
+legend = folium.Element(legend_html)
+legend.add_to(m)
 
-# Generate and display random data for latitude, longitude, type of crime, and time of crime
-for _ in range(50):
-    lat = random.uniform(ls1, ld1)
-    long = random.uniform(ls2, ld2)
-
-    # Generate random type of crime
-    crime_types = ["Robbery", "Assault", "Burglary", "Theft", "Vandalism", "Fraud"]
-    crime_type = random.choice(crime_types)
-
-    # Generate a random time of crime (you can modify this as needed)
-    hour = random.randint(0, 23)
-    minute = random.randint(0, 59)
-    time_of_crime = f"{hour:02d}:{minute:02d}"
-
-    # Store the data in the list
-    crime_data.append([lat, long, crime_type, time_of_crime])
-
-    location = f"Latitude: {lat}, Longitude: {long}, Type of Crime: {crime_type}, Time of Crime: {time_of_crime}"
-
-    # Add markers to the map
-    folium.Marker([lat, long], icon=folium.Icon(color='gray'), popup=location).add_to(m)
-
-# Calculate the mean (average) latitude and longitude of the random data
-mean_latitude = sum(entry[0] for entry in crime_data) / len(crime_data)
-mean_longitude = sum(entry[1] for entry in crime_data) / len(crime_data)
-
-current_mean_location = f"Current Mean Location: Latitude {mean_latitude}, Longitude {mean_longitude}"
-
-# Add a marker for the current mean location
-folium.Marker([mean_latitude, mean_longitude], icon=folium.Icon(color='orange'), popup=current_mean_location).add_to(m)
-
-# Draw an orange circle around the current mean location to indicate a danger zone
-folium.Circle(location=[mean_latitude, mean_longitude], radius=len(crime_data) * (abs(ls1 - ld1)) / (abs(ls2 - ld2)), color='orange', fill=True, fill_color='orange').add_to(m)
-
-# Calculate the overall mean by considering both the existing and random data
-existing_latitudes = existing_data["Latitude"]
-existing_longitudes = existing_data["Longitude"]
-
-# Calculate the mean (average) of the existing latitude and longitude
-existing_mean_latitude = existing_latitudes.mean()
-existing_mean_longitude = existing_longitudes.mean()
-
-overall_mean_latitude = (existing_mean_latitude + mean_latitude) / 2
-overall_mean_longitude = (existing_mean_longitude + mean_longitude) / 2
-overall_mean_location = f"Overall Mean Location: Latitude {overall_mean_latitude}, Longitude {overall_mean_longitude}"
-
-# Add a marker for the overall mean location
-folium.Marker([overall_mean_latitude, overall_mean_longitude], icon=folium.Icon(color='red'), popup=overall_mean_location).add_to(m)
-
-# Draw a red circle around the overall mean location to indicate a danger zone (1000m radius)
-folium.Circle(location=[overall_mean_latitude, overall_mean_longitude], radius=len(existing_data) * (abs(ls1 - ld1)) / (abs(ls2 - ld2)), color='red', fill=True, fill_color='red').add_to(m)
-
-# Save the updated data back to the CSV file
-updated_data = pd.concat([existing_data, pd.DataFrame(crime_data, columns=["Latitude", "Longitude", "Type of Crime", "Time of Crime"])], ignore_index=True)
-updated_data.to_csv("crime_data.csv", index=False)
-
-# Create a client for the OpenRouteService API (you need to sign up for an API key)
 client = Client(key='5b3ce3597851110001cf62487728600986d14f3a8c4bce2711134303')
 
 # Get the route between the source and destination coordinates
 route = client.directions(coordinates=[[ls2, ls1], [ld2, ld1]], profile='driving-car', format='geojson')
 
-# Create a GeoJSON layer for the route
 route_layer = folium.GeoJson(route, name='Route')
-
-# Add the route layer to the map
 route_layer.add_to(m)
 
-# Check if the route passes through the red circle area
-route_coordinates = route["features"][0]["geometry"]["coordinates"]
-for coord in route_coordinates:
-    route_lat, route_lon = coord
-    # Calculate the distance from the route point to the overall mean location
-    distance = (abs(overall_mean_latitude - route_lat) * 111.32) + (abs(overall_mean_longitude - route_lon) * 111.32)
-    
-    # Check if the distance is less than the radius of the red circle (in meters)
-    if distance < (len(existing_data) * (abs(ls1 - ld1)) / (abs(ls2 - ld2))):
-        alert_message = "Alert: Route passes through a danger zone!"
-        folium.Marker([route_lat, route_lon], icon=folium.Icon(color='red'), popup=alert_message).add_to(m)
-        print(alert_message)
+# Extract route coordinates
+route_coordinates = route['features'][0]['geometry']['coordinates']
 
-# Add Layer Control to toggle layers on/off
-folium.LayerControl().add_to(m)
-m.save("route_map.html")
+# Create a Polygon from the route coordinates to define the geofence
+route_polygon = Polygon(route_coordinates)
+
+# Generate random data for points
+points_within_route = []
+
+for _ in range(100):
+    lat = random.uniform(ls1, ld1)
+    long = random.uniform(ls2, ld2)
+    point = Point(long, lat)
+    
+    # Check if the point is within the geofence
+    if route_polygon.contains(point):
+        points_within_route.append(point)
+
+# Add markers for points within the route
+for point in points_within_route:
+    lat, lon = point.y, point.x
+    folium.CircleMarker(
+        location=[lat, lon],
+        radius=5,
+        color='red',
+        fill=True,
+        fill_color='red',
+        fill_opacity=0.7,
+    ).add_to(m)
+
+# Create a GeoJSON layer for the geofence
+folium.GeoJson(route_polygon, name='Geofence', style_function=lambda x: {'color': 'blue'}).add_to(m)
+
+# Save the map to an HTML file
+m.save("source_to_destination_route_with_geofence_legend.html")
 
